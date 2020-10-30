afe7a39908dbcd96d710a89b993cbe2d3bcc67c6
Lilith <themrblacksapce@hotmail.com>
2020-10-30 10:57:47 -0700

    setup project with phaser-template and webpack

---
+++ src/Scenes/GameScene.js
@@ -0,0 +1,20 @@
+import 'phaser';
+import Player from '../Entities/Player';
+import PlayerShot from '../Entities/PlayerShot';
+import Enemy1 from '../Entities/Enemy1';
+import EnemyShot from '../Entities/EnemyShot';
+
+export default class GameScene extends Phaser.Scene {
+  constructor () {
+    super('Game');
+  }
+
+  preload () {
+  }
+
+  create () {
+  }
+
+  update () {
+  }
+};
